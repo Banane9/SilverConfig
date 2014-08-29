b099a99d26d154c7d42652430ae6af327c8ad9a7
Arne Kiesewetter <banane1996@gmx.net>
2014-08-29 23:38:29 +0200

    Add Base Attributes and project stuff.

---
+++ SilverConfig/SilverConfigAttribute.cs
@@ -0,0 +1,20 @@
+﻿using SilverConfig.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SilverConfig
+{
+    /// <summary>
+    /// Marks a class as being a SilverConfig serializable class.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = false, Inherited = true)]
+    public sealed class SilverConfigAttribute : Attribute
+    {
+        /// <summary>
+        /// The name that the Class will be serialized to (if applicable).
+        /// </summary>
+        [CanBeNull, UsedImplicitly]
+        public string Name { get; set; }
+    }
+}
---
+++ SilverConfig/SilverConfigElementAttribute.cs
@@ -0,0 +1,40 @@
+﻿using SilverConfig.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SilverConfig
+{
+    /// <summary>
+    /// Marks a Field or Property as having to be serialized in a <see cref="SilverConfigAttribute"/> class.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public sealed class SilverConfigElementAttribute : Attribute
+    {
+        /// <summary>
+        /// The comment that will be written over the Element.
+        /// </summary>
+        [CanBeNull, UsedImplicitly]
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// The index that the Element will have in the serialized Config.
+        /// The order of colliding indexes will be determined by how they come up.
+        /// Will be sorted in Ascending (0, 1, 2, ...) order.
+        /// </summary>
+        [UsedImplicitly]
+        public uint Index { get; set; }
+
+        /// <summary>
+        /// The name that the Field or Property will be serialized to.
+        /// </summary>
+        [CanBeNull, UsedImplicitly]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Whether there will be an empty line before the Element in the serialized Config or not.
+        /// </summary>
+        [UsedImplicitly]
+        public bool NewLineBefore { get; set; }
+    }
+}
